555e52b65c064cab618fa8188c1c92d404503aaa
Thiago Antenor Linux <thiagoantenor31@gmail.com>
2024-11-08 11:12:52 -0300

    fix: abre o navegador e um link apenas

--- WikipediaPythonLink.py
+++ WikipediaPythonLink.py
@@ -1,25 +1,27 @@
 #!./my-venv/bin/python3
 # WikipediaPythonLink.py
 # Utiliza do selenium para abrir o Chrome, no artigo do Linux no Wikipedia e entra em todos os links
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
-
+from selenium import webdriver;
+from selenium.webdriver.common.keys import Keys;
+from selenium.webdriver.common.by import By;
+from selenium.webdriver.common.action_chains import ActionChains;
+import time;
 #from selenium.webdriver.chrome.service import Service
 #from webdriver_manager.chrome import ChromeDriverManager
 
 #service = Service(ChromeDriverManager().install())
-driver = webdriver.Chrome()
-driver.get("https://pt.wikipedia.org/wiki/Linux")
 
-self.assertIn("Linux – Wikipédia, a enciclopédia livre",
-               driver.title)
+driver = webdriver.Chrome();
+driver.get("https://pt.wikipedia.org/wiki/Linux");
 
-links = driver.find_elements_by_tag_name("a")
+assert("Linux – Wikipédia, a enciclopédia livre" in driver.title);
+
+links = ['Linux (desambiguação)', 'sistemas operacionais', 'kernel', 'Linus Torvalds'];
+
 for link in links:
-    url = link.get_attribute("href");
-    driver.get(url)
-    print(url);
+    url =driver.find_element(By.LINK_TEXT, link).click();
+    time.sleep(5);
+    driver.close();
+
     
-driver.close()
 
